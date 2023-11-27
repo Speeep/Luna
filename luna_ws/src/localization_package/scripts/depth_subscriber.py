ef84a5bcfe7a16d7b7c60e74ef3ca3535e437dec
Giovanni Giacalone <gbgiacalone@wpi.edu>
2023-11-27 18:50:10 -0500

    Implemented color mapping to depth image

--- luna_ws/src/localization_package/scripts/depth_subscriber.py
+++ luna_ws/src/localization_package/scripts/depth_subscriber.py
@@ -7,8 +7,11 @@ def depth_image_callback(msg):
     bridge = CvBridge()
     depth_image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
-    # Display the depth image
-    cv2.imshow("Depth Image", depth_image)
+    # Apply colormap to visualize depth information
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+    # Display the depth image with colormap
+    cv2.imshow("Depth Image", depth_colormap)
     cv2.waitKey(1)
 
 def main():
