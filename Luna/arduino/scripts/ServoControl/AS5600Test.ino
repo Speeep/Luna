f7617be3f34ddf849a3a5cee43a5d7e5e9a28476
Byrnezy <bsbyrne26@gmail.com>
2023-12-01 13:43:13 -0500

    created the magnetic coder into a publishing ros node

---
+++ Luna/arduino/scripts/ServoControl/AS5600Test.ino
@@ -0,0 +1,129 @@
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Wire.h>
+#include <ros.h>
+#include <std_msgs/Int32.h>
+
+ros::NodeHandle nh;
+
+Wire wire;
+
+std_msgs::Int32 Magnetic_pos_msg;
+ros::Publisher Magnetic_pos_pub("magnetic_pos", &Magnetic_pos_msg);
+
+//Magnetic sensor things
+int magnetStatus = 0; //value of the status register (MD, ML, MH)
+
+int lowbyte; //raw angle 7:0
+word highbyte; //raw angle 7:0 and 11:8
+int rawAngle; //final raw angle 
+float degAngle; //raw angle in degrees (360/4096 * [value between 0-4095])
+
+int quadrantNumber, previousquadrantNumber; //quadrant IDs
+float numberofTurns = 0; //number of turns
+float correctedAngle = 0; //tared angle - based on the startup value
+float startAngle = 0; //starting angle
+float totalAngle = 0; //total absolute angular displacement
+float previoustotalAngle = 0; //for the display printing
+
+void ReadRawAngle()
+{ 
+  //7:0 - bits
+  Wire.beginTransmission(0x36); //connect to the sensor
+  Wire.write(0x0D); //figure 21 - register map: Raw angle (7:0)
+  Wire.endTransmission(); //end transmission
+  Wire.requestFrom(0x36, 1); //request from the sensor
+  
+  while(Wire.available() == 0); //wait until it becomes available 
+  lowbyte = Wire.read(); //Reading the data after the request
+ 
+  //11:8 - 4 bits
+  Wire.beginTransmission(0x36);
+  Wire.write(0x0C); //figure 21 - register map: Raw angle (11:8)
+  Wire.endTransmission();
+  Wire.requestFrom(0x36, 1);
+  
+  while(Wire.available() == 0);  
+  highbyte = Wire.read();
+  
+  //4 bits have to be shifted to its proper place as we want to build a 12-bit number
+  highbyte = highbyte << 8; //shifting to left
+  //What is happening here is the following: The variable is being shifted by 8 bits to the left:
+  //Initial value: 00000000|00001111 (word = 16 bits or 2 bytes)
+  //Left shifting by eight bits: 00001111|00000000 so, the high byte is filled in
+  
+  //Finally, we combine (bitwise OR) the two numbers:
+  //High: 00001111|00000000
+  //Low:  00000000|00001111
+  //      -----------------
+  //H|L:  00001111|00001111
+  rawAngle = highbyte | lowbyte; //int is 16 bits (as well as the word)
+
+  //We need to calculate the angle:
+  //12 bit -> 4096 different levels: 360° is divided into 4096 equal parts:
+  //360/4096 = 0.087890625
+  //Multiply the output of the encoder with 0.087890625
+  degAngle = rawAngle * 0.087890625;
+  
+  //Serial.print("Deg angle: ");
+  //Serial.println(degAngle, 2); //absolute position of the encoder within the 0-360 circle
+  
+}
+
+void correctAngle()
+{
+  degAngle  = (int)degAngle % 360;
+  //Serial.print("Corrected angle: ");
+  //Serial.println(correctedAngle, 2); //print the corrected/tared angle  
+}
+
+void checkMagnetPresence()
+{  
+  //This function runs in the setup() and it locks the MCU until the magnet is not positioned properly
+
+  while((magnetStatus & 32) != 32) //while the magnet is not adjusted to the proper distance - 32: MD = 1
+  {
+    magnetStatus = 0; //reset reading
+
+    Wire.beginTransmission(0x36); //connect to the sensor
+    Wire.write(0x0B); //figure 21 - register map: Status: MD ML MH
+    Wire.endTransmission(); //end transmission
+    Wire.requestFrom(0x36, 1); //request from the sensor
+
+    while(Wire.available() == 0); //wait until it becomes available 
+    magnetStatus = Wire.read(); //Reading the data after the request
+
+    //Serial.print("Magnet status: ");
+    //Serial.println(magnetStatus, BIN); //print it in binary so you can compare it to the table (fig 21)      
+  }   
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //start serial - tip: don't use serial if you don't need it (speed considerations)
+  Wire.begin(); //start i2C  
+	Wire.setClock(800000L); //fast clock
+  checkMagnetPresence(); //check the magnet (blocks until magnet is found)
+  ReadRawAngle();
+  startAngle = degAngle; //update startAngle with degAngle - for taring
+
+  nh.initNode(); //make a reading so the degAngle gets updated
+  nh.advertise(Magnetic_pos_pub);  // Advertise the magnetic_pos topic
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:  
+  ReadRawAngle(); //ask the value from the sensor
+  correctAngle(); //tare the value
+  
+  // Publish the magnetic sensor data to the ROS topic
+  Magnetic_pos_msg.data = static_cast<int32_t>(degAngle);
+  Magnetic_pos_pub.publish(&Magnetic_pos_msg);
+  
+  nh.spinOnce();
+  delay(1);
+}
