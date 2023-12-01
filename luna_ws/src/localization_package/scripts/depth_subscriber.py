52055b52105159bfaa107aa27f3ed3a260a4ee05
Byrnezy <bsbyrne26@gmail.com>
2023-11-30 20:13:50 -0500

    taking off drivetrain.ino

--- luna_ws/src/localization_package/scripts/depth_subscriber.py
+++ luna_ws/src/localization_package/scripts/depth_subscriber.py
@@ -7,11 +7,8 @@ def depth_image_callback(msg):
     bridge = CvBridge()
     depth_image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
-    # Apply colormap to visualize depth information
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-    # Display the depth image with colormap
-    cv2.imshow("Depth Image", depth_colormap)
+    # Display the depth image
+    cv2.imshow("Depth Image", depth_image)
     cv2.waitKey(1)
 
 def main():
