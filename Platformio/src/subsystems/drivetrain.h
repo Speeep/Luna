be38e8894ab2d1414c9cb004498927cf864eb6c5
ianmacinerney <ianmacinerney@gmail.com>
2024-01-23 17:34:39 -0500

    Started drivertrain tools
    Set up drivetrain.h to handle new tools

--- Platformio/src/subsystems/drivetrain.h
+++ Platformio/src/subsystems/drivetrain.h
@@ -5,6 +5,7 @@
 #include "./peripheral/snowblower.h"
 #include "./peripheral/encoder.h"
 #include "./peripheral/CANController.h"
+#include <geometry_msgs/Pose.h>
 
 class Drivetrain {
  public:
@@ -51,18 +52,38 @@ class Drivetrain {
 
   String getSums();
 
+  void ICCWheelAngles(float icc);
+
+  void OdometryStep(float wheelSpeeds[4], float wheelAngles[2]);
+
  private:
     Snowblower left_turn_motor;
     Snowblower right_turn_motor;
     Encoder left_wheelpod_encoder;
     Encoder right_wheelpod_encoder;
     CANController can_controller;
+
     bool enabled;
+
     float leftWheelpodAngleSetpoint;
     float rightWheelpodAngleSetpoint;
+
     float leftWheelpodAngle;
     float rightWheelpodAngle;
+
     int turnMotorEffort;
+
     bool isAngled;
+
     float driveSpeed;
+    
+    float width = 53.34; //cm
+    float length = 50.8; //cm
+
+    long previousOdomTime; //ms
+    
+    geometry_msgs::Pose currentPose; //cm and quaternion
+
+    float leftICCAngle; //rad
+    float rightICCAngle; //rad
 };
