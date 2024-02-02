134a668798455ad0b36bb1c75ec8f786c2794b87
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-02-01 20:51:45 -0500

    Implemented Complementary filter

--- luna_ws/src/localization_package/src/localization_filter.py
+++ luna_ws/src/localization_package/src/localization_filter.py
@@ -1,14 +1,28 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 
-pose_step = [0.0, 0.0, 0.0]
+# Constants for filter tuning
+alpha = 0.2  # Weight for localization estimates
+beta = 0.8   # Weight for pose steps
+
+pose = (0.0, 0.0, 0.0)
+pose_step = (0.0, 0.0, 0.0)
+localization_estimate = (0.0, 0.0, 0.0)
+odom_timeout = 1.0
+localizer_timeout = 10.0
+last_odom_time = rospy.Time(0)
+last_localization_time = rospy.Time(0)
 
 
 def update_odom_data_cb(odom_msg):
+    global pose_step, last_odom_time
     pose_step = odom_msg.data
-    print(len(pose_step))
-    print(type(pose_step))
-    # print(pose_step)
+    last_odom_time = rospy.Time.now()
+
+def update_localization_estimate_cb(localization_estimate_msg):
+    global localization_estimate, last_localization_time
+    localization_estimate = localization_estimate_msg.data
+    last_localization_time = rospy.Time.now()
 
 def filter():
 
@@ -16,7 +30,36 @@ def filter():
 
     rospy.Subscriber('/jetson/pose_step', Float32MultiArray, update_odom_data_cb)
 
-    rospy.spin()
+    rospy.Subscriber('jetson/localization_estimate', Float32MultiArray, update_localization_estimate_cb)
+
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        current_time = rospy.Time.now()
+
+        # If there isn't a base pose yet, use 100% localization estimate from webcam
+        if pose == (0.0, 0.0, 0.0):
+
+            # If localization_estimate is also (0.0, 0.0, 0.0) this will result in an infinite loop until timeout.
+            pose = localization_estimate
+
+        # Check for timeouts
+        if (current_time - last_odom_time).to_sec() > odom_timeout or (current_time - last_localization_time).to_sec() > localizer_timeout:
+            rospy.logerr("Timeout occurred!")
+            pose = (0.0, 0.0, 0.0)
+            
+        else:
+
+            # Complementary Filter Here
+            fused_pose = (
+                alpha * localization_estimate[0] + beta * (pose[0] + pose_step[0]),
+                alpha * localization_estimate[1] + beta * (pose[1] + pose_step[1]),
+                alpha * localization_estimate[2] + beta * (pose[2] + pose_step[2])
+            )
+
+            # Update the pose
+            pose = fused_pose
+        
+        rate.sleep()  
 
 if __name__ == '__main__':
     filter()
