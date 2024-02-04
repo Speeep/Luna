6a34ab1ed7684067729118d2152ccb7f8ee8ee7d
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-02-04 13:36:41 -0500

    implemented a median filter for drivetrain wheel speeds

--- Platformio/src/peripheral/CANController.cpp
+++ Platformio/src/peripheral/CANController.cpp
@@ -151,10 +151,10 @@ void CANController::updateMotorSpeeds() {
     float motor2Speed = motor2deltaAngle / deltaTime;
     float motor3Speed = motor3deltaAngle / deltaTime;
 
-    speeds[0] = motor0Speed;
-    speeds[1] = motor1Speed;
-    speeds[2] = motor2Speed;
-    speeds[3] = motor3Speed;
+    speeds[0] = mf0.filter(motor0Speed);
+    speeds[1] = mf1.filter(motor1Speed);
+    speeds[2] = mf2.filter(motor2Speed);
+    speeds[3] = mf3.filter(motor3Speed);
 
     // Update all last timestep values
     lastTime = time;
