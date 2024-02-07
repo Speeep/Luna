Fixed quaternion for filtered_robot_pose

luna_ws/src/localization_package/src/localization_filter.py
@@ -90,13 +90,17 @@ def filter():
             # Update the pose
             pose = fused_pose
 
-            # Define a basic point that we can apply all the tfs to
             filtered_robot_pose = PoseStamped()
             filtered_robot_pose.header = Header()
             filtered_robot_pose.header.stamp = rospy.Time.now()
             filtered_robot_pose.pose = Pose()
-            filtered_robot_pose.pose.position = Point(pose[0], pose[1], 0.0)
-            quat = tf.transformations.quaternion_from_euler(float(0.0),float(0.0),float(pose[2]))
+            filtered_robot_pose.pose.position = Point(float(pose[0]), float(pose[1]), float(0.0))
+            quat = Quaternion()
+            quat_vals = tf.transformations.quaternion_from_euler(float(0.0), float(0.0), float(pose[2]))
+            quat.x = quat_vals[0]
+            quat.y = quat_vals[1]
+            quat.z = quat_vals[2]
+            quat.w = quat_vals[3]
             filtered_robot_pose.pose.orientation = quat
             filtered_pose_pub.publish(filtered_robot_pose)
 