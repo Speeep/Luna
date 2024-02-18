700a45a2dc3a3740152b6516be961e21b5565558
Byrnezy <bsbyrne26@gmail.com>
2024-02-18 17:56:52 -0500

    added in the world_to_grid, grid_to_world, grid_to_index, index_to_grid

--- luna_ws/src/mapping_package/src/map.py
+++ luna_ws/src/mapping_package/src/map.py
@@ -1,104 +1,279 @@
 import math
-import numpy
+import numpy as np
 import rospy
 import tf
-from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import TransformStamped
-
+from nav_msgs.msg import OccupancyGrid, GridCells
+from geometry_msgs.msg import TransformStamped, Point
+from scipy import ndimage
+import functools
 
 # Grid Cell Size
 GRID_CELL_SIZE_M = 0.08
-
-# initialize node
-rospy.init_node('map')
-
-# Initialize occupancy grid message
 map_msg = OccupancyGrid()
 map_msg.header.frame_id = 'map'
 resolution = GRID_CELL_SIZE_M
 width = int(6.88 / GRID_CELL_SIZE_M) # 86
 height = int(5 / GRID_CELL_SIZE_M) # 62
-
-# Map update rate (defaulted to 5 Hz)
+ # Map update rate (defaulted to 5 Hz)
 rate = 5.0
 
-def set_obstacles(grid, x, y, size): # size in m
-    grid[x, y] = int(100)
-    if (size > 0.1):
-        if  grid[x+1, y] < int(1):
-            grid[x+1, y] = int(100)
-        if  grid[x-1, y] < int(1):
-            grid[x-1, y] = int(100)
-        if  grid[x-1, y-1] < int(1):
-            grid[x-1, y-1] = int(100)
-        if  grid[x, y-1] < int(1):
-            grid[x, y-1] = int(100)
-        if grid[x+1, y-1] < int(1):
-            grid[x+1,y-1] = int(100)
-        if  grid[x-1, y+1] < int(1):
-            grid[x-1, y+1] = int(100)
-        if  grid[x, y+1] < int(1):
-            grid[x, y+1] = int(100)
-        if  grid[x+1, y+1] < int(1):
-            grid[x+1, y+1] = int(100)
-        if grid[x+1, y-1] < int(1):
-            grid[x+1,y-1] = int(100)
-        if  grid[x-1, y+1] < int(1):
-            grid[x-1, y+1] = int(100)
-        if  grid[x, y+1] < int(1):
-            grid[x, y+1] = int(100)
-        if  grid[x+1, y+1] < int(1):
-            grid[x+1, y+1] = int(100)
 
-# Publishers
-occ_pub = rospy.Publisher("/robot/map", OccupancyGrid, queue_size = 10)
+class Mapping:
+    def __init__(self):
+        rospy.init_node("map")
+        rospy.Subscriber('/map', OccupancyGrid, self.update_map)
+        
+        # Initialize occupancy grid message
+        self.map_msg = map_msg
+        
+        # fill map_msg with the parameters
+        self.map_msg.info.resolution = resolution
+        self.map_msg.info.width = width
+        map_msg.info.height = height
+        map_msg.data = [0] * (width * height)
+        
+        # initialize grid with -1 (unknown)
+        self.grid = np.ndarray((height, width), buffer=np.zeros((width, height), dtype=np.int),
+                dtype=np.int)
+        self.grid.fill(int(0))
+        
+        for i in range(width):
+            self.grid[0][i] = 100
+            self.grid[height-1][i] = 100
 
-# TF Broadcaster
-tf_broadcaster = tf.TransformBroadcaster()
+        for i in range(height):
+            self.grid[i][0] = 100
+            self.grid[i][width-1] = 100
+
+        # set map origin [meters]
+        map_msg.info.origin.position.x = 0.0
+        map_msg.info.origin.position.y = 0.0
+        
+        # Publishers
+        self.occ_pub = rospy.Publisher("/robot/map", OccupancyGrid, queue_size = 10)
+
+        # TF Broadcaster
+        self.tf_broadcaster = tf.TransformBroadcaster()
+    
+        rospy.sleep(1.0)
+        rospy.loginfo("Mapping node ready")
+
+    def set_obstacles_neighbors(grid, x, y, size): # size in m
+        grid[x, y] = int(100)
+        if (size > (resolution)):
+            if  grid[x+1, y] < int(1):
+                grid[x+1, y] = int(100)
+            if  grid[x-1, y] < int(1):
+                grid[x-1, y] = int(100)
+            if  grid[x-1, y-1] < int(1):
+                grid[x-1, y-1] = int(100)
+            if  grid[x, y-1] < int(1):
+                grid[x, y-1] = int(100)
+            if grid[x+1, y-1] < int(1):
+                grid[x+1,y-1] = int(100)
+            if  grid[x-1, y+1] < int(1):
+                grid[x-1, y+1] = int(100)
+            if  grid[x, y+1] < int(1):
+                grid[x, y+1] = int(100)
+            if  grid[x+1, y+1] < int(1):
+                grid[x+1, y+1] = int(100)
+            if grid[x+1, y-1] < int(1):
+                grid[x+1,y-1] = int(100)
+            if  grid[x-1, y+1] < int(1):
+                grid[x-1, y+1] = int(100)
+            if  grid[x, y+1] < int(1):
+                grid[x, y+1] = int(100)
+            if  grid[x+1, y+1] < int(1):
+                grid[x+1, y+1] = int(100)
+
+    def update_map(self, map_msg):
+        """
+        Requests the map from the map server.
+        :return [OccupancyGrid] The grid if the service call was successful,
+                                None in case of error.
+        """
+        self.map_msg = map_msg
+        self.frontier_detection()
+            
+    @staticmethod
+    def neighbors_of_8(self, map_msg, x, y):
+        """
+        Returns the walkable 8-neighbors cells of (x,y) in the occupancy grid.
+        :param map_msgF [OccupancyGrid] The map information.
+        :param x       [int]           The X coordinate in the grid.
+        :param y       [int]           The Y coordinate in the grid.
+        :return        [[(int,int)]]   A list of walkable 8-neighbors.
+        """
+        neighbors = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+
+        return neighbors
+
+    @staticmethod
+    def world_to_grid(self, x, y):
+        
+        # Grid cell indices for (x, y) point
+        grid_x = int((x - self.map_msg.info.origin.position.x) / map_msg.info.resolution)
+        grid_y = int((y - self.map_msg.info.origin.position.y) / map_msg.info.resolution)
+        
+        point = Point()
+        point.x = grid_x
+        point.y = grid_y
+        point.z = 0.0
+        return point
+
+    @staticmethod
+    def grid_to_world(grid_x, grid_y):
+        # real-world coordinates for given grid cell
+        x = (grid_x * map_msg.info.resolution) + map_msg.info.origin.position.x + (map_msg.info.resolution / 2.0)
+        y = (grid_y * map_msg.info.resolution) + map_msg.info.origin.position.y + (map_msg.info.resolution / 2.0)
+
+        point = Point()
+        point.x = x
+        point.y = y
+        point.z = 0.0
+        return point
+
+    @staticmethod
+    def grid_to_index(map_msg, x, y):
+        """
+        Returns the index corresponding to the given (x,y) coordinates in the occupancy grid.
+        :param x [int] The cell X coordinate.
+        :param y [int] The cell Y coordinate.
+        :return  [int] The index.
+        """
+        return y * map_msg.info.width + x
+        
+    @staticmethod
+    def index_to_grid(map_msg, i):
+        """
+        Returns the index corresponding to the given (x,y) coordinates in the occupancy grid.
+        :param x [int] The cell index.
+        :return  ([int], [int]) The (x,y) coordinate .
+        """
+        x = i % map_msg.info.width
+        y = i // map_msg.info.width
+        return (y, x)
+
+    @staticmethod
+    def is_cell_walkable(self, map_msg, x, y):
+        width  = map_msg.info.width
+        height  = map_msg.info.height
+        map_array = map_msg.data
+        index = self.grid_to_index(map_msg,x,y)
+        boundary_value = 0
+        if(index <= len(map_array) - 1):
+            boundary_value = map_array[self.grid_to_index(map_msg,x,y)]
+        else:
+            boundary_value = -1
+
+        if(x < 0 or y < 0 or x > width or y > height or boundary_value == 100 or boundary_value < 0):
+            return False
+
+        return True
+
+
+    def get_frontier_cells(self):
+            width = self.map_msg.info.width
+            height = self.map_msg.info.height
+            map_array = np.array(self.map_msg.data)
+
+            # reshape into 2D map
+            map_matrix = np.reshape(map_array, (height, width))
+
+            # use sobel to detect large differences between cells
+            sx = ndimage.sobel(map_matrix, axis=0, mode='constant')
+            sy = ndimage.sobel(map_matrix, axis=1 , mode='constant')
+            sob = np.hypot(sx, sy)
+
+            # create filter to remove edges that border boundaries
+            footprint = np.array([[1,1,1],
+                                [1,0,1],
+                                [1,1,1]])
+
+            def boundary_helper(values):
+                return 100 in values
+            
+            boundary_neighbors = ndimage.generic_filter(map_matrix, boundary_helper, footprint=footprint)
+            
+            # filter
+            indices = np.where((sob >= 1) & (map_matrix == 0) & (boundary_neighbors == 0))
+            
+            # convert to array of tuple coordinates
+            indices = np.array(indices)
+            indices = indices.T
+            return list(map(tuple, indices))
+        
+    def publish_grid_cells(self, cells, publisher):
+        # create and publish grid cells message
+        world_cells = [self.grid_to_world(self.msg, i[1], i[0]) for i in cells]
+
+        resolution = self.mapdata.info.resolution
+        msg_grid_cells = GridCells()
+        msg_grid_cells.header.stamp = rospy.Time.now()
+        msg_grid_cells.header.frame_id = "map"
+        msg_grid_cells.cell_height = resolution
+        msg_grid_cells.cell_width = resolution
+        msg_grid_cells.cells = world_cells
+
+        publisher.publish(msg_grid_cells)
+        
+    def segment_frontiers(self, cells):
+        frontiers = []
+        world_cells = [self.grid_to_world(self.map_msg, i[1], i[0]) for i in cells]
+
+        frontier_count = 0
+        while len(cells) > 0:
+            # search neighboring cells to build frontier
+            frontiers.append([])
+            search_queue = []
+            search_queue.append(cells.pop())
+            frontiers[frontier_count] = []
+            while len(search_queue) > 0:
+                cell = search_queue.pop()
+                frontiers[frontier_count].append(cell)
+                x = cell[0]
+                y = cell[1]
+                neighbors = self.neighbors_of_8(self, self.map_msg, x, y)
+                for neighbor in neighbors:
+                    if neighbor in cells:
+                        search_queue.append(neighbor)
+                        cells.remove(neighbor)
+            frontier_count += 1
+        
+        # filter frontiers by size
+        filtered_frontiers = [frontier for frontier in frontiers if len(frontier) > self.FRONTIER_SIZE_TRESHOLD]
+        return filtered_frontiers
+
+    def frontier_detection(self):
+        cell_indices = self.get_frontier_cells()
+        # group nearby frontier cells into arrays
+        frontiers = self.segment_frontiers(cell_indices)
+        
+    def run(self):
+        """
+        Runs the node until Ctrl-C is pressed.
+        """
+        loop_rate = rospy.Rate(rate)
+        while not rospy.is_shutdown():
+
+            # stamp current ros time to the message
+            map_msg.header.stamp = rospy.Time.now()
+            # build ros map message and publish
+            for i in range(width*height):
+                map_msg.data[i] = self.grid.flat[i]
+            self.occ_pub.publish(map_msg)
+
+            # Publish TF transform between "map" and "world" frame
+            self.tf_broadcaster.sendTransform(
+                (0.0, 0.0, 0.0),
+                (0.0, 0.0, 0.0, 1.0),
+                rospy.Time.now(),
+                "map",
+                "world"
+            )
+
+            loop_rate.sleep()
 
 # main function
 if __name__ == '__main__':
-    
-    # fill map_msg with the parameters
-    map_msg.info.resolution = resolution
-    map_msg.info.width = width
-    map_msg.info.height = height
-    map_msg.data = [0] * (width * height)
-
-    # initialize grid with -1 (unknown)
-    grid = numpy.ndarray((height, width), buffer=numpy.zeros((width, height), dtype=numpy.int),
-             dtype=numpy.int)
-    grid.fill(int(0))
-
-    for i in range(width):
-        grid[0][i] = 100
-        grid[height-1][i] = 100
-
-    for i in range(height):
-        grid[i][0] = 100
-        grid[i][width-1] = 100
-
-    # set map origin [meters]
-    map_msg.info.origin.position.x = 0.0
-    map_msg.info.origin.position.y = 0.0
-
-    loop_rate = rospy.Rate(rate)
-    while not rospy.is_shutdown():
-
-        # stamp current ros time to the message
-        map_msg.header.stamp = rospy.Time.now()
-        # build ros map message and publish
-        for i in range(width*height):
-            map_msg.data[i] = grid.flat[i]
-        occ_pub.publish(map_msg)
-
-        # Publish TF transform between "map" and "world" frame
-        tf_broadcaster.sendTransform(
-            (0.0, 0.0, 0.0),
-            (0.0, 0.0, 0.0, 1.0),
-            rospy.Time.now(),
-            "map",
-            "world"
-        )
-
-        loop_rate.sleep()
+    Mapping.run()
