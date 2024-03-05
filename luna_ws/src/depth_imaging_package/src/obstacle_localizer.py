6dc0db2037a02d38fd80a6fef9a773a5783064e9
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-03-05 17:14:18 -0500

    Fixed realsense to robot

--- luna_ws/src/depth_imaging_package/src/obstacle_localizer.py
+++ luna_ws/src/depth_imaging_package/src/obstacle_localizer.py
@@ -4,6 +4,11 @@ import numpy as np
 from math import cos, sin, sqrt, pi
 
 # Define global variables
+
+REALSENSE_OFFSET_X = 0.125
+REALSENSE_OFFSET_Y = 0.215
+REALSENSE_OFFSET_Z = 1.08
+
 x = 0
 y = 0
 theta = 0
@@ -12,16 +17,16 @@ obstacle_location_realsense = [0, 0, 0, 0]
 
 # Define callback functions
 def update_obstacle_pose(data):
-    global obstacle_location_realsense
+    global obstacle_location_realsense, REALSENSE_OFFSET_X, REALSENSE_OFFSET_Y, REALSENSE_OFFSET_Z
     obstacle_location_realsense = data.data
 
     point_realsense = np.array([obstacle_location_realsense[2], -obstacle_location_realsense[0], -obstacle_location_realsense[1], 1])
 
     realsense_to_robot_tf = np.array([
-        [0.707, 0, -0.707, 0.076],
-        [0, 1, 0, -0.048],
-        [0.707, 0, 0.707, -0.076],
-        [0, 0, 0, 1]
+        [0.707, 0,  -0.707, REALSENSE_OFFSET_X],
+        [0,     1,  0,      REALSENSE_OFFSET_Y],
+        [0.707, 0,  0.707,  REALSENSE_OFFSET_Z],
+        [0,     0,  0,      1]
     ])
 
     point_robot = np.dot(realsense_to_robot_tf, point_realsense)
