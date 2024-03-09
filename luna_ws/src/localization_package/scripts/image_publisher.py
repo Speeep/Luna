02af0e533e72b18ee6c03eb78be24a5576fc3678
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-03-08 21:31:42 -0500

    Fixed localizer sweeper maybe again

--- luna_ws/src/localization_package/scripts/image_publisher.py
+++ luna_ws/src/localization_package/scripts/image_publisher.py
@@ -96,6 +96,8 @@ def main():
             marker_corners, ids, _ = aruco.detectMarkers(gray_frame, dictionary)
             height, width, channels = frame.shape
 
+            print("getting here")
+
             # Draw detected markers on the frame.
             if ids is not None:
 
@@ -181,6 +183,7 @@ def main():
                     servo_error_publisher.publish(servo_error)
 
             else: 
+                print("getting all the way here")
                 servo_error_publisher.publish(0.0001)
 
             # Convert the OpenCV image to a ROS Image message
