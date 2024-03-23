df1b9b057108e4b450e49b0726b69fdd1363bb63
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-03-23 17:57:06 -0400

    Arduino Not Working

--- luna_ws/src/control_package/src/keyboard_control.py
+++ luna_ws/src/control_package/src/keyboard_control.py
@@ -121,6 +121,10 @@ class KeyControlNode:
             self.localizer_enable = True
             localizer_enable.data = self.localizer_enable
 
+        # TODO Remove after testing
+        self.drivetrain_state = 0
+        self.localizer_enable = False
+
         state.data = self.drivetrain_state
         self.drivetrain_state_pub.publish(state)
         localizer_enable.data = self.localizer_enable
