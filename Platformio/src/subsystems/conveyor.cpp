1f8b2206560e62b08244b216ba8fa399efd1f750
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-03-26 02:26:22 -0400

    All systems working

--- Platformio/src/subsystems/conveyor.cpp
+++ Platformio/src/subsystems/conveyor.cpp
@@ -59,11 +59,11 @@ void Conveyor::loop() {
     // signal goes low when a limit is hit
     if(plungeSpeed > 0 && digitalRead(PLUNGE_BOT) == HIGH){
         // Run motor to plunge down
-        effort = 6;
+        effort = 10;
     }
     else if(plungeSpeed < 0 && digitalRead(PLUNGE_TOP) == HIGH){
         // Run motor to plunge down
-        effort = -6;
+        effort = -10;
     }
     else{
         effort = 0;
