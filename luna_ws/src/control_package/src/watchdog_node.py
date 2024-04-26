5dbe31de8853f41611eeb8145d8c79cc659a6bc4
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-04-26 01:06:00 -0400

    Implementing watchdog timer

--- luna_ws/src/control_package/src/watchdog_node.py
+++ luna_ws/src/control_package/src/watchdog_node.py
@@ -15,7 +15,7 @@ def watchdog():
     rospy.init_node('rosserial_watchdog')
     rospy.Subscriber("/watchdog_bool", Bool, callback)
     rate = rospy.Rate(3)  # check 3 times every second
-    wait_rate = rospy.Rate(0.2)
+    # wait_rate = rospy.Rate(0.2)
     sleep(5)
     last_time_received = rospy.get_time()
 
@@ -24,12 +24,12 @@ def watchdog():
         rospy.loginfo(rospy.get_time() - last_time_received)
 
         if rospy.get_time() - last_time_received > 1:  # 1 second without update
-            rospy.logwarn("No updates from rosserial_python, restarting node!")
+            rospy.logerr("No updates from rosserial_python, restarting node!")
             subprocess.call(["rosnode", "kill", "/serial_node"])
-            rospy.loginfo("Relaunching Ros Serial Arduino Node")
+            # rospy.loginfo("Relaunching Ros Serial Arduino Node")
             subprocess.Popen(["roslaunch", "robot_package", "serial_node.launch"])
-            rospy.loginfo("Done relaunching Arduino Node!")
-            wait_rate.sleep() # Sleep to let the node relaunch
+            # rospy.loginfo("Done relaunching Arduino Node!")
+            sleep(5) # Sleep to let the node relaunch
             last_time_received = rospy.get_time() # Reset time to prevent auto retriggers
         rate.sleep()
 
