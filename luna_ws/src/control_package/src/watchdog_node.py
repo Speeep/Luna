282344fa497d57a1b2213b49d9f7934694156338
Giovanni Giacalone <gbgiacalone@wpi.edu>
2024-04-26 00:40:36 -0400

    Implementing watchdog timer

--- luna_ws/src/control_package/src/watchdog_node.py
+++ luna_ws/src/control_package/src/watchdog_node.py
@@ -3,17 +3,23 @@ from std_msgs.msg import Bool
 import subprocess
 from time import sleep
 
+# Init last time received
+last_time_received = 0
+
 def callback(data):
     global last_time_received
     last_time_received = rospy.get_time()
     print(f"Most recent time is: {last_time_received}")
 
 def watchdog():
+    global last_time_received
     rospy.init_node('rosserial_watchdog')
     rospy.Subscriber("/watchdog_bool", Bool, callback)
-    last_time_received = rospy.get_time()
     rate = rospy.Rate(3)  # check 3 times every second
-    sleep(10)
+    wait_rate = rospy.Rate(0.2)
+    sleep(5)
+    last_time_received = rospy.get_time()
+
     while not rospy.is_shutdown():
 
         print(rospy.get_time() - last_time_received)
@@ -22,7 +28,7 @@ def watchdog():
             print("No updates from rosserial_python, restarting node...")
             subprocess.call(["rosnode", "kill", "/serial_node"])
             subprocess.call(["roslaunch", "robot_package", "serial_node.launch"])
-            sleep(10) # Sleep to let the node relaunch
+            wait_rate.sleep() # Sleep to let the node relaunch
             last_time_received = rospy.get_time() # Reset time to prevent auto retriggers
         rate.sleep()
 
